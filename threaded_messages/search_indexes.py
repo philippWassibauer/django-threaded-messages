387b6b766e8c55e1a354041e2688d076b2cbb260
Philipp Wassibauer <phil@gidsy.com>
2011-12-14 15:57:04 +0100

    updating search index to 2.0 haystack

--- threaded_messages/search_indexes.py
+++ threaded_messages/search_indexes.py
@@ -1,17 +1,18 @@
-import datetime
-from haystack.indexes import *
-from haystack import site
+from haystack import indexes
 from models import Thread
 
-class ThreadIndex(RealTimeSearchIndex):
-    text = CharField(document=True, use_template=True)
-    participants = MultiValueField()
-    last_message = DateTimeField(model_attr='latest_msg__sent_at')
+class ThreadIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    participants = indexes.MultiValueField()
+    last_message = indexes.DateTimeField(model_attr='latest_msg__sent_at')
 
     def index_queryset(self):
         return Thread.objects.all()
     
     def prepare_participants(self, object):
         return [p.user.pk for p in object.participants.all()]
+
+    def get_model(self):
+        return Thread
         
-site.register(Thread, ThreadIndex)
+
